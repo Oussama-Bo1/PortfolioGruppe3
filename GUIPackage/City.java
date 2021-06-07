6a113755adbbb14c8d67865c20e19b7433244c85
Soufiane461 <79450257+Soufiane461@users.noreply.github.com>
2021-06-07 11:20:32 +0200

    Added TestView

---
+++ GUIPackage/City.java
@@ -0,0 +1,21 @@
+package Postleitzahl;
+
+public class City {
+    private String name;
+    private int plz;
+
+    public City(String name, int plz) {
+        this.name = name;
+        this.plz = plz;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPLZ() {
+        return plz;
+    }
+
+}
+
