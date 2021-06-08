600431990227cd0bae70c26ae531a55c27c1a6a9
Oussama-Bo1 <79406559+Oussama-Bo1@users.noreply.github.com>
2021-06-08 21:30:49 +0200

    Deleted Unecessary files and folders

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,13 +1,26 @@
 package Controller;
 import Model.DatabaseManager;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class Controller {
 
-        public List<String> Countries(String[] args) {
-            DatabaseManager dbm = new DatabaseManager();
-            List<String> countrie = dbm.getAvailableCountries();
-            return countrie;
+        private String Ort = "Frankfurt am Main";
+        private String PLZ;
+        private String Country;
+        DatabaseManager dbm = new DatabaseManager();
+        private List<String> Countries = dbm.getAvailableCountries();
+        HashMap<String,List<String>> plzs = dbm.getPostcodes(Country);
+
+        public String getChosenCountry() {
+            for (String cities : plzs)
+                System.out.println(cities);
         }
-    }
+
+        public List<String> getPostCodes() {
+            List<String> PLZ = plzs.get(Ort);
+            return PLZ;
+        }
+}
