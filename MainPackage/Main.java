315abb61c1c31a2b954ee326f42aac5b1c524f39
Oussama-Bo1 <79406559+Oussama-Bo1@users.noreply.github.com>
2021-06-09 13:57:43 +0200

    Newest Version for Main Branch

---
+++ MainPackage/Main.java
@@ -0,0 +1,12 @@
+package MainPackage;
+
+import Controller.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Controller myController = new Controller();
+
+    }
+}
